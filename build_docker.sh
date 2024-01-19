4d4cc7899f3018312526d8087d0f902a2f24dc8f
Dmytro Semenets <dmytro_semenets@epam.com>
2024-01-19 17:47:28 +0200

    Remove existing docker image before build
    Signed-off-by: Dmytro Semenets <dmytro_semenets@epam.com>

--- build_docker.sh
+++ build_docker.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+HASH=`docker image ls | awk '{ if ($1 == "zephyr_tester") print $3; }'`
+if [ -n "$HASH" ]; then
+    docker image rm $HASH
+fi
 docker build . -f Dockerfile --build-arg "USER_ID=$(id -u)" --build-arg "USER_GID=$(id -g)" -t zephyr_tester:v1
